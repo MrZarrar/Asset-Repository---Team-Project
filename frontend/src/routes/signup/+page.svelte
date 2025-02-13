7dc4fc80eb5940d359be5c02008140fdb2f7c323
Andre Mohammed <andre.mohammed.2023@live.rhul.ac.uk>
2025-02-13 20:32:29 +0000

    #15 added register function for signup page

---
+++ frontend/src/routes/signup/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import pb from '$lib/pocketbase';
+
+    let email = "";
+    let password = "";
+    let confirmPassword = "";
+
+    async function register() {
+        if (password !== confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+    }
+</script>
+
