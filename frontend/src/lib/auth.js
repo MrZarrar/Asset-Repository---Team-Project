86cfd5f0a9772336e0f89184144ed5d81c32d3a7
Andre Mohammed <andre.mohammed.2023@live.rhul.ac.uk>
2025-02-20 14:40:05 +0000

    #15 Added functionality to the SignUp method
    This function allows individuals to sign up using an email and a password as of right now. I will later on add more parameters so that it would match up with the profile settings

--- frontend/src/lib/auth.js
+++ frontend/src/lib/auth.js
@@ -1,6 +1,23 @@
 import { pb } from '$lib/pocketbase';
 
 export async function SignUp(email, password, passwordConfirm){
+    try {
+        if (password != passwordConfirm){
+            throw new Error("The Passwords don't match")
+        }
+        
+        const user = await pb.collection('users').create(
+            email,
+            password,
+            passwordConfirm
+        )
+
+        return user;
+        
+    } catch (error) {
+        console.error("Sign-up error:", error);
+        throw error;        
+    }
 
 }
 
