13dc9b7161f4d60563ce4ca9785856b3da3d5e84
Andre Mohammed <andre.mohammed.2023@live.rhul.ac.uk>
2025-02-20 14:20:37 +0000

    #15 Added basic function for the authentication
    I realised that I had made the functions within the routes folder where it would have been better to put them in lib folder and refernce all the functions I will be adding in the future here

---
+++ frontend/src/lib/auth.js
@@ -0,0 +1,14 @@
+import { pb } from '$lib/pocketbase';
+
+export async function SignUp(email, password, passwordConfirm){
+
+}
+
+
+export async function login(email, password) {
+
+}
+
+export function logout() {
+
+}
