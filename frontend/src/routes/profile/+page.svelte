ac66ce94e7c9a12e6ea03559b84a8e046675825b
Daniel <wmis063@live.rhul.ac.uk>
2025-02-21 00:20:23 +0000

    #46 Added proper link conncetion between Profile and Home pages

--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -65,7 +65,7 @@
 							Profile
 						</a>
 						<a
-							href="/home"
+							href="/"
 							class="rounded-md px-3 py-2 text-sm font-medium text-black dark:text-gray-300 hover:bg-gray-300 dark:hover:bg-gray-700 hover:scale-105 transition-all duration-300"
 						>
 							Dashboard
@@ -98,7 +98,7 @@
 		<div class={`${isMobileMenuOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu">
 			<div class="space-y-1 px-2 pt-2 pb-3">
 				<a
-					href="/home"
+					href="/+page.svelte"
 					class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
 					aria-current="page"
 				>
