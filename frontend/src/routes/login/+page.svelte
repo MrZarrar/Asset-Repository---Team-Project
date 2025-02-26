e32081c7db5ab03bc2874b19b7950c79660eb6c7
Andre Mohammed <andre.mohammed.2023@live.rhul.ac.uk>
2025-02-26 19:47:01 +0000

    pb_data
    We should really implement a .gitignore for these files as they are temporary files

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { logIn } from '$lib/auth'; 
+    
+
+    let email = "";
+    let password = "";
+    let confirmPassword = "";
+
+    async function handleLoginUp() {
+        try {
+            await logIn(email, password);
+            alert("Log-in successful! You can now log in.");
+        } catch (error) {
+            errorMessage = error.message;
+        }
+    }
+    
+</script>
+
+<h1>Login Up</h1>
+<form on:submit|preventDefault={handleLoginUp}>
+    <input type="email" bind:value={email} placeholder="Email" required />
+    <input type="password" bind:value={password} placeholder="Password" required />
+    <button type="submit">Sign Up</button>
+</form>
