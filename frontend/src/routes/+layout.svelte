aa9649e7b59d01c1422ef193f7c9d1d7fe820bea
zlac170 <zlac170@live.rhul.ac.uk>
2025-03-17 14:18:22 +0000

    #50 user session is now working

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script>
  import Chatbot from "$lib/components/Chatbot.svelte";
 import Navigation from "$lib/components/Navigation.svelte";
+import { onMount } from 'svelte';
+import { initAuth, authStore } from '$lib/auth';
+import { page } from '$app/stores';
+import pb from '$lib/pocketbase';
 
+// intialise auth on appp start
+onMount(() => {
+    initAuth();
+      pb.authStore.onChange(() => {
+      initAuth();
+    });
+});
 </script>
 
 <Navigation/>
