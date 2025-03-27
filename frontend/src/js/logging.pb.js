8d147a74e4f84a2f39ae3e99e5b29a9f7dc41438
Vasudevan Adrian (2023) ZMAC659 <adrian.vasudevan.2023@live.rhul.ac.uk>
2025-03-27 17:52:56 +0000

    fixed log creation issues

--- frontend/src/js/logging.pb.js
+++ frontend/src/js/logging.pb.js
@@ -5,7 +5,7 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 export async function logActions(action, asset, user) {
     try {
         const logEntry = await pb.collection('logs').create({
-            action, asset, file
+            action, asset, user
         });
         return logEntry; // Return the created entry for potential use
     } catch (error) {
